9c481416d6f43ec3955468636f4a85759e1a87f3
Ethan Kunis <EPKunis@student.fullsail.edu>
2022-06-08 20:35:21 -0400

    Back up kind works

--- cMain.cpp
+++ cMain.cpp
@@ -11,33 +11,72 @@ cMain::cMain() : wxFrame(nullptr, wxID_ANY, "Calculator", wxPoint(30, 30), wxSiz
 {
 	txt = new wxTextCtrl(this, wxID_ANY, "", wxPoint(10, 10), wxSize(200, 100));
 
-	btn = new wxButton(this, 1, "1", wxPoint(10, 140), wxSize(50, 50));
-	btn = new wxButton(this, 2, "2", wxPoint(60, 140), wxSize(50, 50));
-	btn = new wxButton(this, 3, "3", wxPoint(110, 140), wxSize(50, 50));
-	btn = new wxButton(this, 4, "4", wxPoint(10, 200), wxSize(50, 50));
-	btn = new wxButton(this, 5, "5", wxPoint(60, 200), wxSize(50, 50));
-	btn = new wxButton(this, 6, "6", wxPoint(110, 200), wxSize(50, 50));
-	btn = new wxButton(this, 7, "7", wxPoint(10, 260), wxSize(50, 50));
-	btn = new wxButton(this, 8, "8", wxPoint(60, 260), wxSize(50, 50));
-	btn = new wxButton(this, 9, "9", wxPoint(110, 260), wxSize(50, 50));
-	btn = new wxButton(this, 10, "+", wxPoint(160, 140), wxSize(50, 50));
-	btn = new wxButton(this, 11, "0", wxPoint(60, 320), wxSize(50, 50));
-	btn = new wxButton(this, 12, "-", wxPoint(160, 200), wxSize(50, 50));
-	btn = new wxButton(this, 13, "=", wxPoint(160, 320), wxSize(50, 50));
-	btn = new wxButton(this, 14, "C", wxPoint(10, 320), wxSize(50, 50));
-	btn = new wxButton(this, 15, "MOD", wxPoint(160, 380), wxSize(50, 50));
-	btn = new wxButton(this, 16, "/", wxPoint(160, 260), wxSize(50, 50));
-	btn = new wxButton(this, 17, "BIN", wxPoint(60, 380), wxSize(50, 50));
-	btn = new wxButton(this, 18, "HEX", wxPoint(110, 380), wxSize(50, 50));
-	btn = new wxButton(this, 19, "DEC", wxPoint(10, 380), wxSize(50, 50));
-	btn = new wxButton(this, 20, "*", wxPoint(110, 320), wxSize(50, 50));
+	Ones_btn = new wxButton(this, 10001, "1", wxPoint(10, 140), wxSize(50, 50));
+	Two_btn = new wxButton(this, 10002, "2", wxPoint(60, 140), wxSize(50, 50));
+	Three_btn = new wxButton(this, 10003, "3", wxPoint(110, 140), wxSize(50, 50));
+	Four_btn = new wxButton(this, 10004, "4", wxPoint(10, 200), wxSize(50, 50));
+	Five_btn = new wxButton(this, 10005, "5", wxPoint(60, 200), wxSize(50, 50));
+	Size_btn = new wxButton(this, 10006, "6", wxPoint(110, 200), wxSize(50, 50));
+	Seven_btn = new wxButton(this, 10007, "7", wxPoint(10, 260), wxSize(50, 50));
+	Eight_btn = new wxButton(this, 10008, "8", wxPoint(60, 260), wxSize(50, 50));
+	Nine_btn = new wxButton(this, 10009, "9", wxPoint(110, 260), wxSize(50, 50));
+	Zero_btn = new wxButton(this, 10010, "+", wxPoint(160, 140), wxSize(50, 50));
+	Plus_btn = new wxButton(this, 10011, "0", wxPoint(60, 320), wxSize(50, 50));
+	Minus_btn = new wxButton(this, 10012, "-", wxPoint(160, 200), wxSize(50, 50));
+	Mult_btn = new wxButton(this, 10013, "=", wxPoint(160, 320), wxSize(50, 50));
+	Div_btn = new wxButton(this, 10014, "C", wxPoint(10, 320), wxSize(50, 50));
+	Equal_btn = new wxButton(this, 10015, "MOD", wxPoint(160, 380), wxSize(50, 50));
+	Clear_btn = new wxButton(this, 10016, "/", wxPoint(160, 260), wxSize(50, 50));
+	Mod_btn = new wxButton(this, 10017, "BIN", wxPoint(60, 380), wxSize(50, 50));
+	Bin_btn = new wxButton(this, 10018, "HEX", wxPoint(110, 380), wxSize(50, 50));
+	Hex_btn = new wxButton(this, 10019, "DEC", wxPoint(10, 380), wxSize(50, 50));
+	Dec_btn = new wxButton(this, 10020, "*", wxPoint(110, 320), wxSize(50, 50));
+
+
+	Ones_btn->Bind(wxEVT_COMMAND_BUTTON_CLICKED, &cMain::OnButtonClicked, this);
+	Two_btn->Bind(wxEVT_COMMAND_BUTTON_CLICKED, &cMain::OnButtonClicked, this);
+
+	/*Two_btn->Bind(wxEVT_COMMAND_BUTTON_CLICKED, &cMain::OnButtonClicked, this);
+	Two_btn->Bind(wxEVT_COMMAND_BUTTON_CLICKED, &cMain::OnButtonClicked, this);
+	Two_btn->Bind(wxEVT_COMMAND_BUTTON_CLICKED, &cMain::OnButtonClicked, this);
+	Two_btn->Bind(wxEVT_COMMAND_BUTTON_CLICKED, &cMain::OnButtonClicked, this);
+	Two_btn->Bind(wxEVT_COMMAND_BUTTON_CLICKED, &cMain::OnButtonClicked, this);
+	Two_btn->Bind(wxEVT_COMMAND_BUTTON_CLICKED, &cMain::OnButtonClicked, this);
+	Two_btn->Bind(wxEVT_COMMAND_BUTTON_CLICKED, &cMain::OnButtonClicked, this); 
+	Two_btn->Bind(wxEVT_COMMAND_BUTTON_CLICKED, &cMain::OnButtonClicked, this);
+	Two_btn->Bind(wxEVT_COMMAND_BUTTON_CLICKED, &cMain::OnButtonClicked, this);
+	Two_btn->Bind(wxEVT_COMMAND_BUTTON_CLICKED, &cMain::OnButtonClicked, this);
+	Two_btn->Bind(wxEVT_COMMAND_BUTTON_CLICKED, &cMain::OnButtonClicked, this);
+	Two_btn->Bind(wxEVT_COMMAND_BUTTON_CLICKED, &cMain::OnButtonClicked, this);
+	Two_btn->Bind(wxEVT_COMMAND_BUTTON_CLICKED, &cMain::OnButtonClicked, this);
+	Two_btn->Bind(wxEVT_COMMAND_BUTTON_CLICKED, &cMain::OnButtonClicked, this);
+	Two_btn->Bind(wxEVT_COMMAND_BUTTON_CLICKED, &cMain::OnButtonClicked, this);
+	Two_btn->Bind(wxEVT_COMMAND_BUTTON_CLICKED, &cMain::OnButtonClicked, this);
+	Two_btn->Bind(wxEVT_COMMAND_BUTTON_CLICKED, &cMain::OnButtonClicked, this);
+	Two_btn->Bind(wxEVT_COMMAND_BUTTON_CLICKED, &cMain::OnButtonClicked, this);*/
 }
 
 cMain::~cMain()
 {
+
 }
 
 void cMain::OnButtonClicked(wxCommandEvent& evt)
 {
+	int id = (evt.GetId() - 10000);
+	std::string tmp = "";
+	if (id == 1)
+	{
+		char num = '1';
+		tmp += num;
+		txt->AppendText(tmp);
+	}
+
+	if (id == 2)
+	{
+		char num = '2';
+		tmp += num;
+		txt->AppendText(tmp);
+	}
 
 }
--- cMain.h
+++ cMain.h
@@ -7,16 +7,32 @@ public:
 	~cMain();
 
 public:
-
-	wxButton* btn = nullptr;
 	wxTextCtrl* txt = nullptr;
 
-	int* nField = nullptr;
-	bool FirstClick = true;
+	wxButton* Ones_btn = nullptr;
+	wxButton* Two_btn = nullptr;
+	wxButton* Three_btn = nullptr;
+	wxButton* Four_btn = nullptr;
+	wxButton* Five_btn = nullptr;
+	wxButton* Size_btn = nullptr;
+	wxButton* Seven_btn = nullptr;
+	wxButton* Eight_btn = nullptr;
+	wxButton* Nine_btn = nullptr;
+	wxButton* Zero_btn = nullptr;
+	wxButton* Plus_btn = nullptr;
+	wxButton* Minus_btn = nullptr;
+	wxButton* Mult_btn = nullptr;
+	wxButton* Div_btn = nullptr;
+	wxButton* Equal_btn = nullptr;
+	wxButton* Clear_btn = nullptr;
+	wxButton* Mod_btn = nullptr;
+	wxButton* Bin_btn = nullptr;
+	wxButton* Hex_btn = nullptr;
+	wxButton* Dec_btn = nullptr;
 
 
+	
 	void OnButtonClicked(wxCommandEvent& evt);
-
 	wxDECLARE_EVENT_TABLE();
 };
 
